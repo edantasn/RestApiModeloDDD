09aa9a3d9b1db970719ad076bd8b178398dc136b
Eduardo Dantas <edantasn@gmail.com>
2021-12-21 15:38:23 -0300

    Criados mappers

---
+++ RestApiModeloDDD.Infrastructure/CrossCutting/Interfaces/IMapperProduto.cs
@@ -0,0 +1,15 @@
+﻿using RestApiModeloDDD.Application.Dtos;
+using RestApiModeloDDD.Domain.Entitys;
+using System.Collections.Generic;
+
+namespace RestApiModeloDDD.Infrastructure.CrossCutting.Interfaces
+{
+    public interface IMapperProduto
+    {
+        Produto MapperDtoToEntity(ProdutoDto produtoDto);
+
+        IEnumerable<ProdutoDto> MapperListProdutosDto(IEnumerable<Produto> produtos);
+
+        Produto MapperEntityToDto(Produto produto);
+    }
+}
