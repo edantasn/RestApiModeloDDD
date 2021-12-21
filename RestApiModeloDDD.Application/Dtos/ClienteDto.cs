195d6eed5479073ae3ef13b65ae8c8b2b5dab984
Eduardo Dantas <edantasn@gmail.com>
2021-12-21 17:26:26 -0300

    Entidades

--- RestApiModeloDDD.Application/Dtos/ClienteDto.cs
+++ RestApiModeloDDD.Application/Dtos/ClienteDto.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RestApiModeloDDD.Application.Dtos
+﻿namespace RestApiModeloDDD.Application.Dtos
 {
     public class ClienteDto
     {
-        public int? Id { get; set; }
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
         public string Email { get; set; }
